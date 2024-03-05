d5afc713de63702432ae06411560650d441214c9
Leonardo Fernandes <147807012+leosife@users.noreply.github.com>
2024-03-05 10:54:21 -0300

    .

---
+++ leo/137_list_comprehension.py
@@ -0,0 +1,12 @@
+produtos = [
+    {'nome':'p1','preco':10},
+    {'nome':'p2','preco':20},
+    {'nome':'p3','preco':30},
+]
+
+novos_produtos = [
+    {**produto, 'preco': produto['preco']*1.2} 
+    if produto['preco'] < 25 else {**produto}
+    for produto in produtos
+]
+print(*novos_produtos, sep='\n')
--- leo/project_fut.py
+++ leo/project_fut.py
@@ -1,9 +1,20 @@
 import random
-las = ['a','b']
 
-les = ['c']
+r1 = ["Time A", "Time B", "Time C", "Time D"]
+r2 = ["Time D", "Time A", "Time B", "Time C"]
+r3 = ["Time A", "Time C", "Time B", "Time D"]
 
 
-las = les
+def simular_confronto(time1, time2):
+    # Gerando um placar aleatório para o confronto
+    placar_time1 = random.randint(0, 5)
+    placar_time2 = random.randint(0, 5)
+    return placar_time1, placar_time2
 
-print(las)
+def resultado(rodada):
+    c = 0
+    for i in range(2):
+        print(rodada[c],' x ',rodada[c+1])
+        c += 2
+
+resultado(r1)
